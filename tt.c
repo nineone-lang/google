3a4e6ed39a9e1f03255396bb14cc523d04d695ea
nineone-lang <you@example.com>
2026-01-27 19:31:36 +0200

    First save of my GCD extraction logic

---
+++ tt.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+      int num1 = 10;
+          int num2 = 5;
+              int extracted_gcd = 5; 
+                                    
+                                       printf("Original: %d/%d\n", num1, num2);
+                                            printf("Simplified: %d/%d\n", num1/extracted_gcd, num2/extracted_gcd);
+                                                 printf("The GCD we extracted was: %d\n", extracted_gcd);
+                                    
+                                                   return 0;
+                                                   }
+                          
